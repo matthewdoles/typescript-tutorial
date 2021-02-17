76e42e201556ca5d7422b1631f25bcb17665f1c5
Matthew Doles <matteddied@gmail.com>
2021-02-16 19:44:42 -0500

    2.5 - Arrays & Tuples

--- typescript-basics/app.ts
+++ typescript-basics/app.ts
@@ -1,9 +1,22 @@
 const person: {
   name: string;
   age: number;
+  hobbies: string[];
+  role: [number, string];
 } = {
   name: 'Matthew',
   age: 25,
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
 
 console.log(person);
+for (const hobby of person.hobbies) {
+  // string methods available to array of strings
+  console.log(hobby.toUpperCase());
+}
+
+// any array example
+let anyArray: any[];
+anyArray = ['sports', 34, false];
+console.log(anyArray);

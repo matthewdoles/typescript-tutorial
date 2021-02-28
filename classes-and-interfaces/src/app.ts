a5e6c246d9525602f50170993c72768a3dfc603d
Matthew Doles <matteddied@gmail.com>
2021-02-28 17:28:33 -0500

    5.2 - "this" keyword & "private" and "public" access modifiers

--- classes-and-interfaces/src/app.ts
+++ classes-and-interfaces/src/app.ts
@@ -1,10 +1,32 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log('Department: ' + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const accounting = new Department('Accounting');
 console.log(accounting);
+accounting.describe();
+
+accounting.addEmployee('Matthew');
+accounting.addEmployee('Max');
+accounting.printEmployeeInformation();
+
+//const accountingCopy = { name: 'COPY', describe: accounting.describe };
+//accountingCopy.describe();

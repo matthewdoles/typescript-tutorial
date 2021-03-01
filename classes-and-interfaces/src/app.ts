76ca82d0a78822624df74db03d89de3f4a444bfe
Matthew Doles <matteddied@gmail.com>
2021-03-01 18:33:04 -0500

    5.4 - Inherticance & "protected" modifier

--- classes-and-interfaces/src/app.ts
+++ classes-and-interfaces/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   // private readonly id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     //this.name = n;
@@ -21,13 +21,54 @@ class Department {
   }
 }
 
-const accounting = new Department('d1', 'Accounting');
+class ITDepartment extends Department {
+  admins: string[];
+
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+  }
+
+  addEmployee(name: string) {
+    if (name === 'Matthew') {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReport() {
+    console.log(this.reports);
+  }
+}
+
+// const accounting = new Department('d1', 'Accounting');
+// console.log(accounting);
+// accounting.describe();
+
+const itDepartment = new ITDepartment('d2', ['Matthew']);
+console.log(itDepartment);
+itDepartment.describe();
+
+const accounting = new AccountingDepartment('d1', []);
 console.log(accounting);
 accounting.describe();
 
 accounting.addEmployee('Matthew');
 accounting.addEmployee('Max');
 accounting.printEmployeeInformation();
+
+accounting.addReport('Something went wrong...');
+accounting.printReport();
 
 //const accountingCopy = { name: 'COPY', describe: accounting.describe };
 //accountingCopy.describe();

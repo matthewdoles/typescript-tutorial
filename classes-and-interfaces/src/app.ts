8a44ef692b48207f2da9608d3bffe0c0eadfa029
Matthew Doles <matteddied@gmail.com>
2021-03-02 17:58:47 -0500

    5.8 - Interfaces as Function Types, Optional Parameters & Properties

--- classes-and-interfaces/src/app.ts
+++ classes-and-interfaces/src/app.ts
@@ -1,5 +1,16 @@
+//type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -7,20 +18,29 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 25;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
+    if (this.name) {
+      console.log(phrase + ' ' + this.name);
+    } else {
+      console.log('Hi!');
+    }
   }
 }
 
 let user1: Greetable;
 user1 = new Person('Matthew');
-
 user1.greet('Hi there - I am');
 console.log(user1);
+
+let user2 = new Person();
+user2.greet('Hi there - I am');
+console.log(user2);

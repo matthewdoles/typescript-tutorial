9392acff7a3f39326817ff1ad5761ea6741d4145
Matthew Doles <matteddied@gmail.com>
2021-03-04 21:49:48 -0500

    6.3 - Type Casting & Index Properties

--- advanced-types/src/app.ts
+++ advanced-types/src/app.ts
@@ -97,3 +97,23 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: 'bird', flyingSpeed: 10 });
+
+const userInputElement = <HTMLInputElement>(
+  document.getElementById('user-input')!
+);
+
+const userInputElement2 = document.getElementById(
+  'user-input'
+)! as HTMLInputElement;
+
+userInputElement.value = 'Hi there!';
+
+interface ErrorContainer {
+  // { email: 'Not a valid email', username: 'Must start with a capital character' }
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email',
+  username: 'Must start with a capital character',
+};

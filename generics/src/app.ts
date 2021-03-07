94a06bf7b0de32f4698e681e00bede708bfd606f
Matthew Doles <matteddied@gmail.com>
2021-03-07 17:50:21 -0500

    7.3 - Generic Classes

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -38,6 +38,7 @@ function countAndDescribe<T extends Lengty>(element: T): [T, string] {
 console.log(countAndDescribe('Hi there!'));
 console.log(countAndDescribe(['Sports', 'Cooking']));
 
+// 'keyof' constraint
 function extractAndConvert<T extends object, U extends keyof T>(
   obj: T,
   key: U
@@ -46,3 +47,33 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 console.log(extractAndConvert({ name: 'Matthew' }, 'name'));
+
+// Generic Classes
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) return;
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Matthew');
+textStorage.addItem('Max');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(5);
+numberStorage.addItem(8);
+numberStorage.removeItem(8);
+console.log(textStorage.getItems());

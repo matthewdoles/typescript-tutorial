5cb6759feec0bc60ae1335170440836933d2c244
Matthew Doles <matteddied@gmail.com>
2021-03-07 17:22:07 -0500

    7.2 - Another Generic Function & 'keyof' Constraint

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,3 +1,4 @@
+// Example Generics
 const names: Array<string> = [];
 
 const promise: Promise<string> = new Promise((resolve) => {
@@ -6,11 +7,12 @@ const promise: Promise<string> = new Promise((resolve) => {
   }, 2000);
 });
 
+// Generic Functions
 promise.then((data) => {
   data.split(' ');
 });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
@@ -18,3 +20,29 @@ console.log(merge({ name: 'Matthew' }, { age: 25 }));
 
 const mergedObj = merge({ name: 'Matthew' }, { age: 25 });
 console.log(mergedObj.name);
+
+interface Lengty {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengty>(element: T): [T, string] {
+  let descriptionText = 'No value';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.';
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe(['Sports', 'Cooking']));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return 'Value: ' + obj[key];
+}
+
+console.log(extractAndConvert({ name: 'Matthew' }, 'name'));

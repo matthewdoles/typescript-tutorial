c32c5767d2617a83f15217e86252e4841c62b785
Matthew Doles <matteddied@gmail.com>
2021-03-08 19:40:21 -0500

    8.2 - Building Useful Decorators & Multiple Decorators

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -5,12 +5,25 @@ function Logger(logString: string) {
   };
 }
 
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    console.log('Rendering template');
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h2')!.textContent = p.name;
+    }
+  };
+}
+
 @Logger('LOGGING - PERSON')
+@WithTemplate('<h2>My Person Object</h2>', 'app')
 class Person {
   name = 'Matthew';
 
   constructor() {
-    console.log('Creating person objec...');
+    console.log('Creating person object...');
   }
 }
 
